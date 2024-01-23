4d0e28d991ac0e99b9bff2e63e0ce28b62a3a6ce
Mert <50770862+HeroTheGreat@users.noreply.github.com>
2024-01-23 08:50:24 +0100

    Add files via upload

---
+++ PythonCalculator.py
@@ -0,0 +1,34 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+# taking inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# calculation performed.
+if operation == "+":
+    result = add(num1, num2)
+elif operation == "-":
+    result = subtract(num1, num2)
+elif operation == "*":
+    result = multiply(num1, num2)
+elif operation == "/":
+    result = divide(num1, num2)
+else:
+    result = "Invalid operation."
+
+# result
+print(f"Result: {result}")
